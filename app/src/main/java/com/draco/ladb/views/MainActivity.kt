b8d2d777418f4972fdfc89cda48d7902e82f0ba0
EasyDeveloper <62865742+EasyDev-11436@users.noreply.github.com>
2021-11-10 10:14:11 +0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ladb/views/MainActivity.kt
+++ app/src/main/java/com/draco/ladb/views/MainActivity.kt
@@ -2,7 +2,11 @@ package com.draco.ladb.views
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.app.AlertDialog
 import android.view.*
 import android.view.inputmethod.InputMethod
